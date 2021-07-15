8f62fb3aeb87ec7d344fe66ee357876b9a95f30e
Taegon Kim <taegon_kim@brown.edu>
2021-07-14 21:26:18 -0400

    npm installs

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("HELLO")
+var moment = require('moment'); // require
+moment().format();
+
+console.log('hellooooooo')
+
+
+console.log(moment().startOf('day').fromNow())
+console.log(moment().endOf('day').fromNow())
+
+
+var math = require('mathjs');
+
+console.log('create and manipulate complex numbers')
+const a = math.complex(2, 3)
+console.log(a); // 2 + 3i
+
+
+console.log('this is so fun!')
